583e6ba6b4ba88d591eabc398c93ec3c7a11b4b2
mhsjacoby <margarite.jacoby@colorado.edu>
2020-08-19 15:25:39 -0700

    read in any number of files and all hubs

--- yolov5/detect.py
+++ yolov5/detect.py
@@ -32,9 +32,22 @@ from utils.utils import *
 import cv2
 import datetime
 import time
+import sys
 
 import warnings
 warnings.filterwarnings("ignore")
+
+
+
+def mylistdir(directory, bit='', end=True):
+    filelist = os.listdir(directory)
+    if end:
+        return [x for x in filelist if x.endswith(f'{bit}') and not x.endswith('.DS_Store')]
+    else:
+         return [x for x in filelist if x.startswith(f'{bit}') and not x.endswith('.DS_Store')]
+
+
+
 
 def detect():
 
@@ -100,96 +113,106 @@ if __name__ == '__main__':
 
 	# Loading arg
 	parser.add_argument('-path','--path', default="AA", type=str, help='path of stored data')
-	parser.add_argument('-H','--H_num', default='1', type=int, help='House number: 1,2,3,4,5,6')
-	parser.add_argument('-sta_num','--station_num', default=1, type=int, help='Station Number')
-	parser.add_argument('-sta_col','--station_color', default="B", type=str, help='Station Color')
+	# parser.add_argument('-H','--H_num', default='1', type=int, help='House number: 1,2,3,4,5,6')
+	# parser.add_argument('-sta_num','--station_num', default=1, type=int, help='Station Number')
+	# parser.add_argument('-sta_col','--station_color', default="B", type=str, help='Station Color')
+	parser.add_argument('-hub', '--hub', default='', type=str, help='if only one hub... ')
+	parser.add_argument('-save_location', '--save', default='', type=str, help='location to store files (if different from path')
 
-	parser.add_argument('-start','--start_date_index', default=0, type=int, help='Processing START Date index')
-	# parser.add_argument('-end','--end_date_index', default=-1, type=int, help='Processing END Date index')
+	parser.add_argument('-start_index','--start_date_index', default=0, type=int, help='Processing START Date index')
+	parser.add_argument('-number_files', '--end_date_index', default=10, type=int, help='Number of files to read')
+	parser.add_argument('-img_file_name', '--img_fname', default='', type=str, help='Name of subfolder containing images')
 
 	opt = parser.parse_args()
 	opt.img_size = check_img_size(opt.img_size)
 
 	path = opt.path
-	H_num = opt.H_num
-	station_num = opt.station_num
-	station_color = opt.station_color
+	save_path = opt.save if len(opt.save) > 0 else path
+
 	start_date_index = opt.start_date_index
-	# end_date_index = opt.end_date_index # Not implemented yet
+	end_date_index = opt.end_date_index
 
-	color_index = {"B": "black", "R": "red", "G": "green"}
-	sta_col = color_index[station_color]
+	H_elements = path.strip('/').split('/')[-1].split('-')
+	H_num = H_elements[0]
+	system = H_elements[1]
+	color_index = {"black": "B", "red": "R", "green": "G"}
+	color = color_index[system]
+
+	hubs = [opt.hub] if len(opt.hub) > 0 else mylistdir(path, bit=f'{color}S', end=False)
+	print(f'Hubs: {hubs}')
+
+	for hub in hubs:
+		img_fnames = [opt.img_fname] if len(opt.img_fname) > 0 else mylistdir(os.path.join(path, hub), bit='img', end=False)
+
+		if len(img_fnames) > 1:
+			print(f'Multiple images files found in hub {hub}: {img_fnames}. \n System Exiting.')
+			sys.exit()
+
+		print(f'Reading images from file: {img_fnames[0]}')
+
+		read_root_path = os.path.join(path,hub,img_fnames[0],"*")
+		dates = sorted(glob.glob(read_root_path))[start_date_index:end_date_index]
+		print('Dates: ', [os.path.basename(d) for d in dates])
+
+		save_root_path = os.path.join(save_path,'Inference_DB', hub, 'img_conf')
+		print("save_root_path: ", save_root_path)
+
+		if not os.path.exists(save_root_path):
+			os.makedirs(save_root_path)
+
+		# ================ Move Model loading etc here ================
+		# Initialize
+		device = torch_utils.select_device(opt.device) # Prints "Using CPU"
+
+		# Load model
+		model = torch.load(opt.weights, map_location=device)['model'].float()  # load to FP32
+		model.to(device).eval()
+
+		start = time.time()
+
+		for date_folder_path in dates:
+			date = os.path.basename(date_folder_path)
+			print("Loading date folder: " + date + "...")
+
+			''' Check if Directory is empty '''
+			times = os.listdir(date_folder_path)
+
+			if len(times) == 0: 
+				print("Date folder "+ os.path.basename(date_folder_path) + " is empty")
+			else:
+				# Created day-content placeholder
+				day_fname, day_occupancy, day_conf = [], [], []			
+				date_folder_path = os.path.join(date_folder_path,"*")
+				
+				for time_folder_path in sorted(glob.glob(date_folder_path)):
+					time_f = os.path.basename(time_folder_path)
+					print("Checking time folder: "+ time_f +"...")
+
+					imgs = os.listdir(time_folder_path)
+
+					if len(imgs) == 0:
+						print("Time folder "+ os.path.basename(time_folder_path) + " is empty")
+
+					else:
+						# Update source folder
+						opt.source = time_folder_path # 1 min folder ~ 60 img
+
+						with torch.no_grad():
+							min_fname, min_occ, min_conf = detect() # detect this time folder
+							day_fname.extend(min_fname)
+							day_occupancy.extend(min_occ)
+							day_conf.extend(min_conf)
 
 
+				day_fname = [date_[:11]+date_[11:13]+":"+date_[13:15]+":"+date_[15:17] for date_ in day_fname] # date formatting
+				day_fname = [datetime.datetime.strptime(date_, '%Y-%m-%d %H:%M:%S') for date_ in day_fname] # date formatting
 
-	read_root_path = os.path.join(path,"H%s-%s"%(H_num,sta_col),"%sS%s"%(station_color,station_num),"img-downsized","*")
-	print("read_root_path: ", read_root_path)
-	
-	save_root_path = os.path.join(path,"Inference_DB","H%s-%s"%(H_num,sta_col),"%sS%s"%(station_color,station_num),"img_conf")
-	print("save_root_path: ", save_root_path)
-
-	if not os.path.exists(save_root_path):
-		os.makedirs(save_root_path)
-
-	dates = sorted(glob.glob(read_root_path))
-	dates = dates[start_date_index:]
-	print("Starting from:", os.path.basename(dates[0]))
-	# dates = dates[start_date_index:end_date_index] # Not implemented
+				save_data = np.vstack((day_fname, day_occupancy, day_conf))
+				save_data = np.transpose(save_data)
+				np.savetxt(os.path.join(save_root_path,date+".csv"), save_data, delimiter=',',fmt='%s',header="timestamp,occupancy,confidence",comments='')
 
 
-	# ================ Move Model loading etc here ================
-	# Initialize
-	device = torch_utils.select_device(opt.device) # Prints "Using CPU"
-
-	# Load model
-	model = torch.load(opt.weights, map_location=device)['model'].float()  # load to FP32
-	model.to(device).eval()
-
-	start = time.time()
-
-	for date_folder_path in dates:
-		date = os.path.basename(date_folder_path)
-		print("Loading date folder: " + date + "...")
-
-		''' Check if Directory is empty '''
-		times = os.listdir(date_folder_path)
-
-		if len(times) == 0: 
-			print("Date folder "+ os.path.basename(date_folder_path) + " is empty")
-		else:
-			# Created day-content placeholder
-			day_fname, day_occupancy, day_conf = [], [], []			
-			date_folder_path = os.path.join(date_folder_path,"*")
-			
-			for time_folder_path in sorted(glob.glob(date_folder_path)):
-				time_f = os.path.basename(time_folder_path)
-				print("Checking time folder: "+ time_f +"...")
-
-				imgs = os.listdir(time_folder_path)
-
-				if len(imgs) == 0:
-					print("Time folder "+ os.path.basename(time_folder_path) + " is empty")
-
-				else:
-					# Update source folder
-					opt.source = time_folder_path # 1 min folder ~ 60 img
-
-					with torch.no_grad():
-						min_fname, min_occ, min_conf = detect() # detect this time folder
-						day_fname.extend(min_fname)
-						day_occupancy.extend(min_occ)
-						day_conf.extend(min_conf)
-
-
-			day_fname = [date_[:11]+date_[11:13]+":"+date_[13:15]+":"+date_[15:17] for date_ in day_fname] # date formatting
-			day_fname = [datetime.datetime.strptime(date_, '%Y-%m-%d %H:%M:%S') for date_ in day_fname] # date formatting
-
-			save_data = np.vstack((day_fname, day_occupancy, day_conf))
-			save_data = np.transpose(save_data)
-			np.savetxt(os.path.join(save_root_path,date+".csv"), save_data, delimiter=',',fmt='%s',header="timestamp,occupancy,confidence",comments='')
-
-
-	end = time.time()
-	print("Time taken:",end-start)
+		end = time.time()
+		print(f'Time taken for hub {hub}:,{end-start}')
 
 
