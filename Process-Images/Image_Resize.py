d18b988101fe45ed32bda882240fbdcaa4f35b36
mhsjacoby <margarite.jacoby@colorado.edu>
2021-01-05 09:45:49 -0800

    reduce size of code to generate downsized images

--- Process-Images/Image_Resize.py
+++ Process-Images/Image_Resize.py
@@ -15,13 +15,14 @@ import pandas as pd
 from datetime import datetime
 from PIL import Image
 import json
+from glob import glob
 
 
 from my_functions import *
 from gen_argparse import *
 
 class ImageFile():
-    def __init__(self, sensor, files_dir, house, write_loc, start_date, img_name, home_system, img_sz=32):
+    def __init__(self, sensor, files_dir, house, write_loc, start_date, end_date, img_name, home_system, img_sz=32):
         self.sensor = sensor
         self.original_loc = files_dir   
         self.home = house
@@ -29,6 +30,7 @@ class ImageFile():
         self.img_sz = img_sz
         self.write_path = write_loc
         self.start_date = start_date
+        self.end_date = end_date
         self.img_name = img_name
         self.get_params()  
         self.total_per_day = 86400
@@ -41,22 +43,22 @@ class ImageFile():
         self.write_location = make_storage_directory(os.path.join(self.write_path, self.sensor, f'img-downsized-{self.img_sz}'))
         print(f'resizeing and saving to: {self.write_location}')
 
-    def get_time(self, file_name):
-        fname = file_name.split('_')
-        day_name, time_name = fname[0], fname[1]
-        return day_name, time_name
+    # def get_time(self, file_name):
+    #     fname = file_name.split('_')
+    #     day_name, time_name = fname[0], fname[1]
+    #     return day_name, time_name
 
 
-    def load_image(self, png):
-        im = Image.open(png)
-        im_array = np.array(list(im.getdata()))
-        small_img = im.resize((self.img_sz, self.img_sz), Image.BILINEAR)
-        ave_pxl = np.mean(im_array)
-        return small_img
+    # def load_image(self, png):
+    #     im = Image.open(png)
+    #     im_array = np.array(list(im.getdata()))
+    #     small_img = im.resize((self.img_sz, self.img_sz), Image.BILINEAR)
+    #     ave_pxl = np.mean(im_array)
+    #     return small_img
 
     # def write_summary(self, days):
     #     store_dir = make_storage_dicrectory(os.path.join(self.write_path, self.home, self.sensor, 'Summaries'))
-    #     fname = os.path.join(store_dir, f'{self.home}-{self.sensor}-img-summary.txt')
+    #     fname = os.path.join(store_dir, f'{self.home}-{self.sensor}-img-summary_ds.txt')
     #     with open(fname, 'w+') as writer:
     #         writer.write('hub day %Capt ')
     #         for day in days:
@@ -75,56 +77,86 @@ class ImageFile():
 
     def main(self):
         print(f'Start date: {self.start_date}')
+        print(f'End date: {self.end_date}')
         all_days = sorted(mylistdir(self.path, '2019-', end=False))
-        self.Day_Summary = {d:0 for d in all_days}
+        print('total files ', len(all_days))
+        all_days = [day for day in all_days if day >= self.start_date]
+        all_days = [day for day in all_days if day <= self.end_date]
+        print('final files', len(all_days))
+
+        sensor = self.sensor
+        home = self.home
+        path = self.path
+        write_location = self.write_location
+        img_sz = self.img_sz
+
+        open_im = Image.open
+        bil = Image.BILINEAR
+
+        # self.Day_Summary = {d:0 for d in all_days}
         print(all_days)
 
         for day in all_days:
-            try:
-                if day >= self.start_date:
-                    start = datetime.now()
-                    F = 0
+            start = datetime.now()
+            F = 0
 
-                    print(day)
-                    hours = [str(x).zfill(2) + '00' for x in range(0,24)]
-                    all_mins = sorted(mylistdir(os.path.join(self.path, day)))
-
-                    for hr in hours:
-                        hr_entry = []
-                        this_hr = [x for x in all_mins if x[0:2] == hr[0:2]]
-                        if len(this_hr) > 0:
-                            for minute in sorted(this_hr):
-                                for img_file in sorted(mylistdir(os.path.join(self.path, day, minute))):
-                                    day_time = self.get_time(img_file)
-                                    str_day, str_time = day_time[0], day_time[1]
-                                    if str_day != day:
-                                        print(f'day mismatch! file {img_file} is in day {day}')
-
-                                    im_name = f'{str_day}_{str_time}_{self.sensor}_{self.home}.png'     
-                                    try:
-                                        img_list = self.load_image(os.path.join(self.path, day, minute, img_file))
-                                        if img_list != 0:
-                                            target_dir = make_storage_directory(os.path.join(self.write_location, str_day, str_time[0:4]))
-                                            if not os.path.exists(os.path.join(target_dir, im_name)):
-                                                img_list.save(os.path.join(target_dir, im_name))
-                                                hr_entry.append(str_time)
-                                                self.Day_Summary[day] += 1 
+            print(day)
+            # hours = [str(x).zfill(2) + '00' for x in range(0,24)]
+            # all_mins = sorted(mylistdir(os.path.join(path, day)))
+            all_mins = sorted(glob(os.path.join(path, day, '*')))
+            print('all mins ', len(all_mins))
 
 
-                                    except Exception as e:
-                                        print(f'Pillow error: {e}')
-                            if len(hr_entry) > 0:
-                                print(f'Total entries for {hr} = {len(hr_entry)}')
-                                F += len(hr_entry)
-                            else:
-                                print(f'No images for {day} hour: {hr}')
-                        else:
-                            print(f'No directories for {day} hour: {hr}')
+            # for hr in hours:
+            #     hr_entry = []
+            #     this_hr = [x for x in all_mins if x[0:2] == hr[0:2]]
+            #     if len(this_hr) > 0:
+                    # for minute in sorted(this_hr):
+            for minute in all_mins:
+                minute_name = os.path.basename(minute)
 
-                    end = datetime.now()
-                    print(f'Time to process day {day}: {str(end-start).split(".")[0]}. Number of files: {F}')
-            except Exception as e:
-                print(f'Error with file {img_file}: {e}')
+                # for img_file in mylistdir(os.path.join(path, day, minute)):
+                imgs = glob(os.path.join(minute, '*.png'))
+                # print(minute)
+                # sys.exit()
+                
+                # if len(imgs)>0:
+                #     print(minute)
+
+                for img_file in imgs:
+                    day_time = os.path.basename(img_file).split('_')
+                    str_day, str_time = day_time[0], day_time[1]
+
+                    im_name = f'{str_day}_{str_time}_{sensor}_{home}.png'     
+                    try:
+                        # png = os.path.join(path, day, minute, img_file)
+                        # im = Image.open(img_file)
+                        im = open_im(img_file)
+                        img_list = im.resize((img_sz, img_sz), bil)
+                        
+                        # img_list = im.resize((img_sz, img_sz), Image.BILINEAR)
+                        target_dir = make_storage_directory(os.path.join(write_location, str_day, str_time[0:4]))
+                        img_list.save(os.path.join(target_dir, im_name))
+                        F += 1
+                                # hr_entry.append(str_time)
+                                    # self.Day_Summary[day] += 1 
+
+
+                    except Exception as e:
+                        print(f'Pillow error: {e}')
+
+                    # if len(hr_entry) > 0:
+                    #     print(f'Total entries for {hr} = {len(hr_entry)}')len(hr_entry)
+                    # else:
+                    #     print(f'No images for {day} hour: {hr}')
+                # else:
+                #     print(f'No directories for {day} hour: {hr}')
+
+            end = datetime.now()
+            print(f'Time to process day {day}: {str(end-start).split(".")[0]}. Number of files: {F}')
+
+            # except Exception as e:
+            #     print(f'Error with file {img_file}: {e}')
 
         # try:
         #     self.write_summary(all_days)
@@ -136,7 +168,7 @@ class ImageFile():
 if __name__ == '__main__':
 
     for hub in hubs:
-        print(f' hub: {hub}\n stored: {path}\n house: {H_num}\n write: {save_root}\n start: {start_date}')
+        print(f' hub: {hub}\n stored: {path}\n house: {H_num}\n write: {save_root}\n start: {start_date}\n end: {end_date}')
 
-        I = ImageFile(hub, path, H_num, save_root, start_date, img_name, home_system)
+        I = ImageFile(hub, path, H_num, save_root, start_date, end_date, img_name, home_system)
         I.main()
